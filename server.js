e411d9b7b3a4647d3d1872877c1bb102fc468370
Tanmay <144552425+babyo77@users.noreply.github.com>
2025-01-28 01:41:32 +0530

    JSON

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use(
 );
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json());
 app.post(
   "/upload-image",
   createProxyMiddleware({
